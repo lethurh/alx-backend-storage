8bf3c363807ed70f6c7c55a7add0503507dd0ad6
lethurh <gugulethupraxedis@gmail.com>
2024-10-19 08:35:32 +0000

    6. Add bonus

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- script that lists all bands with Glam rock
+SELECT DISTINCT band_name,
+                IFNULL(`split`, 2020) - `formed` as `lifespan` 
+from metal_bands where FIND_IN_SET('Glam rock', style)
+ORDER by `lifespan` DESC;
